11bac6ece95b253d2947d5c712606ae56569002a
Marcin Poręba <marcin.poreba1995@gmail.com>
2023-08-28 15:01:44 +0200

    add File upload field

--- server.js
+++ server.js
@@ -1,13 +1,14 @@
 const express = require('express');
 const path = require('path');
 const hbs = require('express-handlebars');
+const multer = require('multer');
 
 
 const app = express();
 
 app.engine('.hbs', hbs({
-    layoutsDir: path.join(__dirname, 'views/layouts'), // Specify the layout directory
-    defaultLayout: 'main' // Set 'main.hbs' as the default layout for all routes
+    layoutsDir: path.join(__dirname, 'views/layouts'),
+    defaultLayout: 'main'
 }));
 app.set('view engine', '.hbs');
 
@@ -15,18 +16,38 @@ app.use(express.static(path.join(__dirname, '/public')));
 
 app.use(express.static(path.join(__dirname, '/user')));
 
-// Custom Middleware for /user/ routes
+
 app.use('/user/', (req, res, next) => {
     res.render('forbidden', { layout: false })
 });
+const fileFilter = (req, file, cb) => {
+    const allowedFileTypes = ['.png', '.jpg', '.jpeg', '.gif'];
+    const ext = path.extname(file.originalname);
+    if (allowedFileTypes.includes(ext.toLowerCase())) {
+        cb(null, true);
+    } else {
+        req.fileError = 'Only .png, .jpg, .jpeg, or .gif files are allowed.';
+        cb(null, false);
+    }
+};
 
-// x-www-form-urlencoded service
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'public/uploads');
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname);
+    }
+});
+
+const upload = multer({ fileFilter });
+
+
 app.use(express.urlencoded({ extended: false }));
 
-// form-data service
+
 app.use(express.json());
 
-// Rest of paths
 app.get(['/','/home'], (req, res) => {
     res.render('index')
 });
@@ -51,15 +72,20 @@ app.get('/hello/:name', (req, res) => {
     res.render('hello', { name: req.params.name });
 });
 
-app.post('/contact/send-message', (req, res) => {
+
+app.post('/contact/send-message', upload.single('file'),  (req, res) => {
 
     const { author, sender, title, message } = req.body;
 
-    if(author && sender && title && message) {
-        res.send('The message has been sent!');
+    if(author && sender && title && message && req.file) {
+        const fileName = req.file && req.file.originalname;
+
+
+        res.render('contact', { isSent: true, fileName });
     }
     else {
-        res.send('You can\'t leave fields empty!')
+
+        res.render('contact', { isError: true, fileError: req.fileError  });
     }
 
 });
